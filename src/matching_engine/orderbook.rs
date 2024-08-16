184f3dd609473e2c4e03fd8489aeacfde35b2e06
Audrique <audriquevert@gmail.com>
2024-08-16 19:59:17 +0200

    Initial commit

--- src/matching_engine/orderbook.rs
+++ src/matching_engine/orderbook.rs
@@ -9,8 +9,8 @@ pub enum BidOrAsk {
 
 #[derive(Debug)]
 pub struct Orderbook {
-    asks: HashMap<Decimal, Limit>,
-    bids: HashMap<Decimal, Limit>,
+    pub asks: HashMap<Decimal, Limit>,
+    pub bids: HashMap<Decimal, Limit>,
 }
 
 impl Orderbook {
@@ -132,7 +132,7 @@ impl Orderbook {
 #[derive(Debug)]
 pub struct Limit {
     price: Decimal, // Not f64 because it can lead to inconsistencies when hashing
-    orders: Vec<Order>,
+    pub orders: Vec<Order>,
 }
 
 impl Limit {
@@ -145,7 +145,7 @@ impl Limit {
 
     // this maybe change to a self.total_volume and add the volume when a new order comes
     // and subtract when volume is filled (this way it is more efficient)
-    fn total_volume(&self) -> f64 {
+    pub fn total_volume(&self) -> f64 {
         self.orders
             .iter()
             .map(|order| order.size)
@@ -163,7 +163,7 @@ impl Limit {
 
     //TODO: At this moment self trades are avoided by just ignoring each trader's
     // other trades. This has to be change by not letting you place the order in the first place.
-    fn fill_order(&mut self, market_order: &mut Order) {
+    pub fn fill_order(&mut self, market_order: &mut Order) {
         let mut i = 0; // Index to keep track of the position in the orders vector
 
         while i < self.orders.len() { // a while loop instead of a for loop because we don't always increment i (see the 'true' case in the match)
@@ -194,7 +194,7 @@ impl Limit {
         }
     }
 
-    fn add_order(&mut self, order: Order) {
+    pub fn add_order(&mut self, order: Order) {
         self.orders.push(order);
     }
 
@@ -202,7 +202,7 @@ impl Limit {
     // to cancel an order you have to give the price and the order_id.
     // However, the price is used to get the correct limit (so only in
     // the part of the orderbook for canceling.
-    fn cancel_order(&mut self, order_id: String) {
+    pub fn cancel_order(&mut self, order_id: String) {
         let index = self.orders.iter().position(|a| a.order_id == order_id).unwrap();
         self.orders.remove(index);
 
@@ -210,7 +210,7 @@ impl Limit {
 }
 #[derive(Debug, Clone)]
 pub struct Order {
-    size: f64,
+    pub size: f64,
     bid_or_ask: BidOrAsk,
     trader_id: String,
     order_id: String
@@ -226,130 +226,4 @@ impl Order {
         self.size == 0.0
     }
 
-}
-
-#[cfg(test)]
-pub mod tests {
-    use rust_decimal_macros::dec;
-    use super::*;
-
-
-    #[test]
-    fn check_open_orders() {
-        let mut orderbook = Orderbook::new();
-
-        let price = dec!(10_000.0);
-        let trader_id = "trader_1".to_string();
-
-        let buy_limit_order_a = Order::new(BidOrAsk::Bid, 100.0, trader_id.clone(), "0".to_string());
-        let buy_limit_order_b = Order::new(BidOrAsk::Bid, 100.0, trader_id.clone(), "1".to_string());
-        let sell_limit_order = Order::new(BidOrAsk::Ask, 100.0, "trader_2".to_string(), "2".to_string());
-        orderbook.add_limit_order(dec!(10_000.0), buy_limit_order_a.clone());
-        orderbook.add_limit_order(dec!(9_000.0), buy_limit_order_a);
-        orderbook.add_limit_order(dec!(10_000.0), buy_limit_order_b);
-        orderbook.add_limit_order(dec!(12_000.0), sell_limit_order);
-        let open_trades_trader1 = orderbook.open_orders_for_trader(trader_id);
-        let open_trades_trader2 = orderbook.open_orders_for_trader("trader_2".to_string());
-
-        assert_eq!(open_trades_trader1.len(), 2);
-        assert_eq!(open_trades_trader1.get(&dec!(10_000.0)).unwrap().len(), 2);
-        assert_eq!(open_trades_trader1.get(&dec!(9_000.0)).unwrap().len(), 1);
-
-        assert_eq!(open_trades_trader2.len(), 1);
-        assert_eq!(open_trades_trader2.get(&dec!(12_000.0)).unwrap().len(), 1);
-    }
-
-    #[test]
-    fn cancel_order_from_limit() {
-        let price = dec!(10_000.0);
-        let mut limit = Limit::new(price);
-        let trader_id = "trader_audrique".to_string();
-
-        let buy_limit_order_a = Order::new(BidOrAsk::Bid, 100.0, trader_id.clone(), "0".to_string());
-        let buy_limit_order_b = Order::new(BidOrAsk::Bid, 100.0, trader_id, "1".to_string());
-        limit.add_order(buy_limit_order_a);
-        limit.add_order(buy_limit_order_b);
-        limit.cancel_order("0".to_string());
-        assert_eq!(limit.orders.len(), 1);
-    }
-
-    #[test]
-    fn cancel_order_from_orderbook() {
-        let mut orderbook = Orderbook::new();
-        orderbook.add_limit_order(dec!(500), Order::new(BidOrAsk::Ask, 10.0, "trader1".to_string(), "0".to_string()));
-        orderbook.add_limit_order(dec!(100), Order::new(BidOrAsk::Ask, 10.0, "trader2".to_string(), "1".to_string()));
-        assert_eq!(orderbook.asks.len(), 2);
-        orderbook.cancel_order(BidOrAsk::Ask,dec!(500), "0".to_string());
-        println!("{:?}", orderbook);
-        assert_eq!(orderbook.asks.len(), 1);
-    }
-
-    #[test]
-    fn orderbook_fill_market_order_ask() {
-        let mut orderbook = Orderbook::new();
-        orderbook.add_limit_order(dec!(500), Order::new(BidOrAsk::Ask, 10.0, "trader1".to_string(), "0".to_string()));
-        orderbook.add_limit_order(dec!(100), Order::new(BidOrAsk::Ask, 10.0, "trader2".to_string(), "1".to_string()));
-        orderbook.add_limit_order(dec!(200), Order::new(BidOrAsk::Ask, 10.0, "trader1".to_string(), "2".to_string()));
-        orderbook.add_limit_order(dec!(300), Order::new(BidOrAsk::Ask, 10.0, "trader1".to_string(), "3".to_string()));
-
-        let mut market_order = Order::new(BidOrAsk::Bid, 10.0,"trader2".to_string(), "4".to_string());
-        orderbook.fill_market_order(&mut market_order);
-
-        let ask_limits = orderbook.ask_limits();
-        let matched_limit = ask_limits.get(1).unwrap();//.orders.get(0).unwrap();
-        assert_eq!(market_order.is_filled(), true);
-        assert_eq!(ask_limits.len(), 3);
-        println!("{:?}", orderbook.ask_limits());
-
-    }
-
-    #[test]
-    fn limit_total_volume() {
-        let price = dec!(10_000.0);
-        let mut limit = Limit::new(price);
-        let trader_id = "trader_audrique".to_string();
-
-        let buy_limit_order_a = Order::new(BidOrAsk::Bid, 100.0, trader_id.clone(), "0".to_string());
-        let buy_limit_order_b = Order::new(BidOrAsk::Bid, 100.0, trader_id, "1".to_string());
-        limit.add_order(buy_limit_order_a);
-        limit.add_order(buy_limit_order_b);
-        assert_eq!(limit.total_volume(), 200.0)
-    }
-
-    #[test]
-    fn limit_order_single_fill() {
-        let price = dec!(10_000.0);
-        let mut limit = Limit::new(price);
-        let buy_limit_order = Order::new(BidOrAsk::Bid, 100.0, "trader_audrique".to_string(), "0".to_string());
-
-        limit.add_order(buy_limit_order);
-
-        let mut market_sell_order = Order::new(BidOrAsk::Ask, 99.0, "trader_2".to_string(), "1".to_string());
-
-        limit.fill_order(&mut market_sell_order);
-
-        assert_eq!(market_sell_order.is_filled(), true);
-        assert_eq!(limit.orders.get(0).unwrap().size, 1.0);
-    }
-
-    #[test]
-    fn limit_order_multi_fill() {
-        let price = dec!(10_000.0);
-        let mut limit = Limit::new(price);
-        let buy_limit_order_a = Order::new(BidOrAsk::Bid, 100.0, "trader1".to_string(), "0".to_string());
-        let buy_limit_order_b = Order::new(BidOrAsk::Bid, 100.0, "trader2".to_string(), "1".to_string());
-
-        limit.add_order(buy_limit_order_a);
-        limit.add_order(buy_limit_order_b);
-
-        let mut market_sell_order = Order::new(BidOrAsk::Ask, 100.0, "trader1".to_string(), "2".to_string());
-
-        limit.fill_order(&mut market_sell_order);
-
-        assert_eq!(market_sell_order.is_filled(), true);
-        assert_eq!(limit.orders.get(0).unwrap().is_filled(), false);
-        assert_eq!(limit.orders.len(), 1);
-
-        println!("{:?}", limit);
-    }
 }
---
+++ src/matching_engine/tests.rs
@@ -0,0 +1,127 @@
+use crate::matching_engine::orderbook::{BidOrAsk, Limit, Order, Orderbook};
+
+#[cfg(test)]
+pub mod tests {
+    use rust_decimal_macros::dec;
+    use super::*;
+
+
+    #[test]
+    fn check_open_orders() {
+        let mut orderbook = Orderbook::new();
+
+        let price = dec!(10_000.0);
+        let trader_id = "trader_1".to_string();
+
+        let buy_limit_order_a = Order::new(BidOrAsk::Bid, 100.0, trader_id.clone(), "0".to_string());
+        let buy_limit_order_b = Order::new(BidOrAsk::Bid, 100.0, trader_id.clone(), "1".to_string());
+        let sell_limit_order = Order::new(BidOrAsk::Ask, 100.0, "trader_2".to_string(), "2".to_string());
+        orderbook.add_limit_order(dec!(10_000.0), buy_limit_order_a.clone());
+        orderbook.add_limit_order(dec!(9_000.0), buy_limit_order_a);
+        orderbook.add_limit_order(dec!(10_000.0), buy_limit_order_b);
+        orderbook.add_limit_order(dec!(12_000.0), sell_limit_order);
+        let open_trades_trader1 = orderbook.open_orders_for_trader(trader_id);
+        let open_trades_trader2 = orderbook.open_orders_for_trader("trader_2".to_string());
+
+        assert_eq!(open_trades_trader1.len(), 2);
+        assert_eq!(open_trades_trader1.get(&dec!(10_000.0)).unwrap().len(), 2);
+        assert_eq!(open_trades_trader1.get(&dec!(9_000.0)).unwrap().len(), 1);
+
+        assert_eq!(open_trades_trader2.len(), 1);
+        assert_eq!(open_trades_trader2.get(&dec!(12_000.0)).unwrap().len(), 1);
+    }
+
+    #[test]
+    fn cancel_order_from_limit() {
+        let price = dec!(10_000.0);
+        let mut limit = Limit::new(price);
+        let trader_id = "trader_audrique".to_string();
+
+        let buy_limit_order_a = Order::new(BidOrAsk::Bid, 100.0, trader_id.clone(), "0".to_string());
+        let buy_limit_order_b = Order::new(BidOrAsk::Bid, 100.0, trader_id, "1".to_string());
+        limit.add_order(buy_limit_order_a);
+        limit.add_order(buy_limit_order_b);
+        limit.cancel_order("0".to_string());
+        assert_eq!(limit.orders.len(), 1);
+    }
+
+    #[test]
+    fn cancel_order_from_orderbook() {
+        let mut orderbook = Orderbook::new();
+        orderbook.add_limit_order(dec!(500), Order::new(BidOrAsk::Ask, 10.0, "trader1".to_string(), "0".to_string()));
+        orderbook.add_limit_order(dec!(100), Order::new(BidOrAsk::Ask, 10.0, "trader2".to_string(), "1".to_string()));
+        assert_eq!(orderbook.asks.len(), 2);
+        orderbook.cancel_order(BidOrAsk::Ask,dec!(500), "0".to_string());
+        println!("{:?}", orderbook);
+        assert_eq!(orderbook.asks.len(), 1);
+    }
+
+    #[test]
+    fn orderbook_fill_market_order_ask() {
+        let mut orderbook = Orderbook::new();
+        orderbook.add_limit_order(dec!(500), Order::new(BidOrAsk::Ask, 10.0, "trader1".to_string(), "0".to_string()));
+        orderbook.add_limit_order(dec!(100), Order::new(BidOrAsk::Ask, 10.0, "trader2".to_string(), "1".to_string()));
+        orderbook.add_limit_order(dec!(200), Order::new(BidOrAsk::Ask, 10.0, "trader1".to_string(), "2".to_string()));
+        orderbook.add_limit_order(dec!(300), Order::new(BidOrAsk::Ask, 10.0, "trader1".to_string(), "3".to_string()));
+
+        let mut market_order = Order::new(BidOrAsk::Bid, 10.0,"trader2".to_string(), "4".to_string());
+        orderbook.fill_market_order(&mut market_order);
+
+        let ask_limits = orderbook.ask_limits();
+        let matched_limit = ask_limits.get(1).unwrap();//.orders.get(0).unwrap();
+        assert_eq!(market_order.is_filled(), true);
+        assert_eq!(ask_limits.len(), 3);
+        println!("{:?}", orderbook.ask_limits());
+
+    }
+
+    #[test]
+    fn limit_total_volume() {
+        let price = dec!(10_000.0);
+        let mut limit = Limit::new(price);
+        let trader_id = "trader_audrique".to_string();
+
+        let buy_limit_order_a = Order::new(BidOrAsk::Bid, 100.0, trader_id.clone(), "0".to_string());
+        let buy_limit_order_b = Order::new(BidOrAsk::Bid, 100.0, trader_id, "1".to_string());
+        limit.add_order(buy_limit_order_a);
+        limit.add_order(buy_limit_order_b);
+        assert_eq!(limit.total_volume(), 200.0)
+    }
+
+    #[test]
+    fn limit_order_single_fill() {
+        let price = dec!(10_000.0);
+        let mut limit = Limit::new(price);
+        let buy_limit_order = Order::new(BidOrAsk::Bid, 100.0, "trader_audrique".to_string(), "0".to_string());
+
+        limit.add_order(buy_limit_order);
+
+        let mut market_sell_order = Order::new(BidOrAsk::Ask, 99.0, "trader_2".to_string(), "1".to_string());
+
+        limit.fill_order(&mut market_sell_order);
+
+        assert_eq!(market_sell_order.is_filled(), true);
+        assert_eq!(limit.orders.get(0).unwrap().size, 1.0);
+    }
+
+    #[test]
+    fn limit_order_multi_fill() {
+        let price = dec!(10_000.0);
+        let mut limit = Limit::new(price);
+        let buy_limit_order_a = Order::new(BidOrAsk::Bid, 100.0, "trader1".to_string(), "0".to_string());
+        let buy_limit_order_b = Order::new(BidOrAsk::Bid, 100.0, "trader2".to_string(), "1".to_string());
+
+        limit.add_order(buy_limit_order_a);
+        limit.add_order(buy_limit_order_b);
+
+        let mut market_sell_order = Order::new(BidOrAsk::Ask, 100.0, "trader1".to_string(), "2".to_string());
+
+        limit.fill_order(&mut market_sell_order);
+
+        assert_eq!(market_sell_order.is_filled(), true);
+        assert_eq!(limit.orders.get(0).unwrap().is_filled(), false);
+        assert_eq!(limit.orders.len(), 1);
+
+        println!("{:?}", limit);
+    }
+}
