b74a4cd15c7a698e8ecdb7ecdbf63590bdf2b8b4
Audrique <audriquevert@gmail.com>
2024-08-20 18:23:29 +0200

    Initial commit

--- src/connecting_to_exchanges/deribit_connection.rs
+++ src/connecting_to_exchanges/deribit_connection.rs
@@ -1,6 +1,7 @@
 use std::fs;
 use serde_json::{json, Value};
-use tokio::net::TcpStream;
+use tokio::{net::TcpStream,
+            task};
 use tokio_tungstenite::{connect_async, MaybeTlsStream, WebSocketStream};
 use tokio_tungstenite::tungstenite::Message;
 use futures_util::{StreamExt, SinkExt};
@@ -66,29 +67,23 @@ pub async fn subscribe_to_channel(ws_stream_ref: &mut WebSocketStream<MaybeTlsSt
     ws_stream_ref.send(Message::Text(msg_text)).await.expect("Failed to send message");
 }
 
-// TODO: run this on a different thread or channel or whatever it is called
-// To make sure we minimize the 'missing of updates'
 pub async fn on_incoming_message(ws_stream_ref: &mut WebSocketStream<MaybeTlsStream<TcpStream>>) {
-    while let Some(message) = ws_stream_ref.next().await {
-        match message {
-            Ok(msg) => match msg {
-                Message::Text(text) => {
-                    println!("Received: {}", text);
-                    // Add your logic here to handle the response
-                }
-                Message::Binary(bin) => {
-                    println!("Received binary data: {:?}", bin);
-                }
-                Message::Close(_) => {
-                    println!("Connection closed");
-                    break;
-                }
-                _ => (),
+    loop {
+        match ws_stream_ref.next().await {
+            Some(Ok(msg)) => {
+                // Immediately offload the message processing to another function/task.
+                task::spawn(handle_message(msg));
             },
-            Err(e) => {
-                println!("Error receiving message: {}", e);
+            Some(Err(e)) => {
+                eprintln!("Error receiving message: {:?}", e);
                 break;
             }
-        }
+            None => break, // Exit the loop if the stream is closed.
+        };
     }
+}
+
+//TODO: process the message here such that the receiving of the messages is the least amount blocked
+async fn handle_message(msg: Message) {
+    println!("{:?}", msg);
 }
